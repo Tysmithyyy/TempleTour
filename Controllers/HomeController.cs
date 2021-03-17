d9db7c16e232bf68f9c344eadf7cf798c78d0cc5
haileyhullinger <hh49362@gmail.com>
2021-03-17 09:04:43 -0600

    updated controller 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,17 +12,28 @@ namespace TempleTour.Controllers
 {
     public class HomeController : Controller
     {
+        //set up for repository for the time slots
         private readonly ILogger<HomeController> _logger;
 
         private ITempleTourRepository _repository;
 
+        
+
+        //set up for the db context for the appointments
+        private TempleTourDbContext context { get; set; }
+
         //added to receive the Itimeslotrepostory
-        public HomeController(ILogger<HomeController> logger, ITempleTourRepository repository)
+        public HomeController(ILogger<HomeController> logger, ITempleTourRepository repository, TempleTourDbContext con)
         {
             _logger = logger;
-            //added
+            
+            //added, repository for all avaialble time slots, context for made appointments
             _repository = repository;
+            context = con;
         }
+
+       
+        //VIEW ACTIONS
 
         //index view
         public IActionResult Index()
@@ -55,28 +66,48 @@ namespace TempleTour.Controllers
             return View("Form", appointment);
         }
 
-        //form view
-        public IActionResult Form()
+        //[HttpPost]
+        //public IActionResult Form(Appointment appointment)
+        //{
+            //return View("ViewAppointments", appointment);
+        //}
+
+        //http post to pull up the page with the list of appoitments.
+        //when the "sign up" button is pressed, the appointment is appended ontot the repository or appointments
+        [HttpPost]
+        public IActionResult Form(Appointment appointment)
         {
             
+            //update database if valid, if not, return the asp-validation summary
+            context.Appointments.Add(appointment);
+            context.SaveChanges();
+
+            
+            return View();
+            
+        }
+
+        
+
+        //form view
+        public IActionResult Form()
+            {
+
+                //return the list of all appointments
+                return View();
+            }
+
+        
+
+        //View Appointments view
+        public IActionResult ViewAppointments()
+        {
             //return the list of all appointments
             return View(new AppointmentListViewModel
             {
                 Appointments = _repository.Appointments
                     .OrderBy(t => t.AppointmentId)
             });
-        }
-
-        [HttpPost]
-        public IActionResult Form(Appointment appointment)
-        {
-            return View("ViewAppointments", appointment);
-        }
-
-        //View Appointments view
-        public IActionResult ViewAppointments()
-        {
-            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
